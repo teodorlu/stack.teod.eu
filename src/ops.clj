eca92dc63ceecdc9d025d68e879058be0f91e3a1
Teodor Heggelund <git@teod.eu>
2025-07-05 14:41:39 +0200

    New backup after joining previous weeknotes into state
    Also require notes explaning why we wanted a backup next to the backup.

--- src/ops.clj
+++ src/ops.clj
@@ -1,17 +1,27 @@
 (ns ops
   (:require
    [babashka.fs :as fs]
-   [babashka.process :as p])
+   [babashka.process :as p]
+   [clojure.string :as str])
   (:import (java.time LocalDate)))
 
-(defn backup-to [folder]
+(def trim+newline #(str (str/trim %) \n))
+
+(defn backup! [notes folder]
+  (when (empty? notes)
+    (throw (ex-info "Notes carry *why* we did a backup just now. Empty notes are not allowed."
+                    {:notes notes})))
   (fs/create-dirs folder)
+  (spit (fs/file folder "NOTES.txt") (trim+newline notes))
   (p/shell {:in (str "get duratom.edn " folder "/duratom.edn")}
            "garden sftp"))
 
 (comment
 
+  ;; Run this from a local REPL to backup.
+  ;; Archive is written to archive/ with a date and timestamp.
+  ;; The intent is to check backups into Git.
   (def archive (fs/file "archive" (str (LocalDate/now) "-" (System/currentTimeMillis))))
-  (backup-to archive)
+  (backup! "New backup after joining weeknotes into impulse/state" archive)
 
   )
